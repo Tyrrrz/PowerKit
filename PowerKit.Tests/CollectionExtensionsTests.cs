be2073fb6e2b134c64ea8936d088cb18051f883e
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-12 23:02:55 +0300

    Update CollectionExtensionsTests.cs

--- PowerKit.Tests/CollectionExtensionsTests.cs
+++ PowerKit.Tests/CollectionExtensionsTests.cs
@@ -11,7 +11,7 @@ public class CollectionExtensionsTests
     public void RemoveAll_Test()
     {
         // Arrange
-        ICollection<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+        var collection = (ICollection<int>) new List<int> { 1, 2, 3, 4, 5 };
 
         // Act
         collection.RemoveAll(x => x % 2 == 0);
