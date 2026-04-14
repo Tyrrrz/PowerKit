f1612c0abe582a77dae9039c26541b8e401c53c1
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-14 20:46:28 +0300

    Update EncodingExtensions.cs

--- PowerKit/Extensions/EncodingExtensions.cs
+++ PowerKit/Extensions/EncodingExtensions.cs
@@ -2,15 +2,18 @@ using System.Text;
 
 namespace PowerKit.Extensions;
 
+file static class EncodingEx
+{
+    public static Encoding Utf8WithoutBom { get; } = new UTF8Encoding(false);
+}
+
 internal static class EncodingExtensions
 {
-    private static readonly Encoding Utf8WithoutBomValue = new UTF8Encoding(false);
-
     extension(Encoding)
     {
         /// <summary>
         /// Gets an instance of the UTF-8 encoding that does not emit a byte order mark (BOM).
         /// </summary>
-        public static Encoding Utf8WithoutBom => Utf8WithoutBomValue;
+        public static Encoding Utf8WithoutBom => EncodingEx.Utf8WithoutBom;
     }
 }
