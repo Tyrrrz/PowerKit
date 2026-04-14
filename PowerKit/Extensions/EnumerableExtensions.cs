619dfd08747e152ea86662608310756c714d3667
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-14 13:58:51 +0300

    Apply suggestion from @Tyrrrz

--- PowerKit/Extensions/EnumerableExtensions.cs
+++ PowerKit/Extensions/EnumerableExtensions.cs
@@ -127,9 +127,9 @@ internal static class EnumerableExtensions
         public T? ElementAtOrNull(int index)
         {
 #if NET40_OR_GREATER || NETSTANDARD || NET
-            IReadOnlyList<T> list = source as IReadOnlyList<T> ?? source.ToArray();
+            var list = source as IReadOnlyList<T> ?? source.ToArray();
 #else
-            IList<T> list = source.ToArray();
+            var list = source.ToArray();
 #endif
             return index >= 0 && index < list.Count ? list[index] : null;
         }
