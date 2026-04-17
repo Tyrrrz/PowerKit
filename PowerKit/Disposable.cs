47e2565636767b870ffe9de787f2ca6c8ec63e3b
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-17 15:22:35 +0300

    Update Disposable.cs

--- PowerKit/Disposable.cs
+++ PowerKit/Disposable.cs
@@ -5,19 +5,18 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace PowerKit;
 
+file class DelegateDisposable(Action dispose) : IDisposable
+{
+    public void Dispose() => dispose();
+}
+
 /// <summary>
 /// Provides utility methods for creating and composing <see cref="IDisposable" /> instances.
 /// </summary>
 #if !POWERKIT_INCLUDE_COVERAGE
 [ExcludeFromCodeCoverage]
 #endif
-internal partial class Disposable(Action dispose) : IDisposable
-{
-    /// <inheritdoc />
-    public void Dispose() => dispose();
-}
-
-internal partial class Disposable
+internal static class Disposable
 {
     /// <summary>
     /// Gets a disposable that performs no action when disposed.
@@ -27,7 +26,7 @@ internal partial class Disposable
     /// <summary>
     /// Creates a disposable that invokes the specified action when disposed.
     /// </summary>
-    public static IDisposable Create(Action dispose) => new Disposable(dispose);
+    public static IDisposable Create(Action dispose) => new DelegateDisposable(dispose);
 
     /// <summary>
     /// Creates a disposable that disposes all specified disposables when disposed,
