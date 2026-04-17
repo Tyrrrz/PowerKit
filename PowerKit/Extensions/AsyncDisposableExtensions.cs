432d516f9c07f42cc86a8e00a195e39300d2bce3
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-17 15:24:06 +0300

    Update AsyncDisposableExtensions.cs

--- PowerKit/Extensions/AsyncDisposableExtensions.cs
+++ PowerKit/Extensions/AsyncDisposableExtensions.cs
@@ -6,31 +6,31 @@ using System.Threading.Tasks;
 
 namespace PowerKit.Extensions;
 
+// Provides a dynamic and uniform way to deal with async disposable.
+// Used as an abstraction to dynamically polyfill IAsyncDisposable implementations in BCL types. For example:
+// - Stream class on .NET Framework 4.6.1 -> calls Dispose()
+// - Stream class on .NET Core 3.0 -> calls DisposeAsync()
+// - Stream class on .NET Standard 2.0 -> calls DisposeAsync() or Dispose(), depending on the runtime
+file class AsyncDisposableAdapter(IDisposable target) : IAsyncDisposable
+{
+    public async ValueTask DisposeAsync()
+    {
+        if (target is IAsyncDisposable asyncDisposable)
+        {
+            await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+        }
+        else
+        {
+            target.Dispose();
+        }
+    }
+}
+
 #if !POWERKIT_INCLUDE_COVERAGE
 [ExcludeFromCodeCoverage]
 #endif
 internal static class AsyncDisposableExtensions
 {
-    // Provides a dynamic and uniform way to deal with async disposable.
-    // Used as an abstraction to polyfill IAsyncDisposable implementations in BCL types. For example:
-    // - Stream class on .NET Framework 4.6.1 -> calls Dispose()
-    // - Stream class on .NET Core 3.0 -> calls DisposeAsync()
-    // - Stream class on .NET Standard 2.0 -> calls DisposeAsync() or Dispose(), depending on the runtime
-    private readonly struct AsyncDisposableAdapter(IDisposable target) : IAsyncDisposable
-    {
-        public async ValueTask DisposeAsync()
-        {
-            if (target is IAsyncDisposable asyncDisposable)
-            {
-                await asyncDisposable.DisposeAsync().ConfigureAwait(false);
-            }
-            else
-            {
-                target.Dispose();
-            }
-        }
-    }
-
     extension(IDisposable disposable)
     {
         /// <summary>
