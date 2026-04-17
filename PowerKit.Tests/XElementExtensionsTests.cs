24d3d47cb6eac80bbb913a63cd64f2c91c0b2280
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-17 21:03:50 +0300

    Update XElementExtensionsTests.cs

--- PowerKit.Tests/XElementExtensionsTests.cs
+++ PowerKit.Tests/XElementExtensionsTests.cs
@@ -9,26 +9,16 @@ namespace PowerKit.Tests;
 public class XElementExtensionsTests
 {
     [Fact]
-    public void StripNamespaces_NoNamespaces_Test()
-    {
-        // Arrange
-        var element = XElement.Parse("<root><child foo=\"bar\" /></root>");
-
-        // Act
-        var result = element.StripNamespaces();
-
-        // Assert
-        result.Name.LocalName.Should().Be("root");
-        result.Descendants().Should().ContainSingle(d => d.Name.LocalName == "child");
-        result.Descendants().Single().Attribute("foo")!.Value.Should().Be("bar");
-    }
-
-    [Fact]
-    public void StripNamespaces_WithNamespaces_Test()
+    public void StripNamespaces_Test()
     {
         // Arrange
         var element = XElement.Parse(
-            "<root xmlns=\"http://example.com\" xmlns:x=\"http://x.com\"><x:child x:foo=\"bar\" /></root>"
+            """
+            <root xmlns="http://example.com" xmlns:x="http://x.com">
+                <x:child x:foo="bar" />
+                <otherChild foo="baz" />
+            </root>
+            """
         );
 
         // Act
@@ -45,19 +35,5 @@ public class XElementExtensionsTests
                 d.Name.NamespaceName.Should().BeEmpty();
                 d.Attributes().Should().AllSatisfy(a => a.Name.NamespaceName.Should().BeEmpty());
             });
-    }
-
-    [Fact]
-    public void StripNamespaces_DoesNotModifyOriginal_Test()
-    {
-        // Arrange
-        var element = XElement.Parse("<root xmlns=\"http://example.com\"><child /></root>");
-        var originalXml = element.ToString();
-
-        // Act
-        element.StripNamespaces();
-
-        // Assert
-        element.ToString().Should().Be(originalXml);
     }
 }
