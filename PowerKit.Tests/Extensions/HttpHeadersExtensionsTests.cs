20415da88a2c4c0e1a9ad493cdd2214dc29c4a01
tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2026-04-19 11:28:48 +0300

    Clean up tests

--- PowerKit.Tests/Extensions/HttpHeadersExtensionsTests.cs
+++ PowerKit.Tests/Extensions/HttpHeadersExtensionsTests.cs
@@ -10,36 +10,30 @@ public class HttpHeadersExtensionsTests
     [Fact]
     public void GetValuesOrEmpty_Test()
     {
+        // Arrange
         using var request = new HttpRequestMessage();
-
-        // Single value
         request.Headers.Add("X-Custom", "value");
-        request.Headers.GetValuesOrEmpty("X-Custom").Should().Equal("value");
-
-        // Missing header returns empty sequence
-        request.Headers.GetValuesOrEmpty("X-Missing").Should().BeEmpty();
-
-        // Multiple values are returned individually
         request.Headers.Add("X-Multi", "foo");
         request.Headers.Add("X-Multi", "bar");
+
+        // Act & assert
+        request.Headers.GetValuesOrEmpty("X-Missing").Should().BeEmpty();
+        request.Headers.GetValuesOrEmpty("X-Custom").Should().Equal("value");
         request.Headers.GetValuesOrEmpty("X-Multi").Should().Equal("foo", "bar");
     }
 
     [Fact]
     public void TryGetValue_Test()
     {
+        // Arrange
         using var request = new HttpRequestMessage();
-
-        // Single value
         request.Headers.Add("X-Custom", "value");
-        request.Headers.TryGetValue("X-Custom").Should().Be("value");
-
-        // Missing header
-        request.Headers.TryGetValue("X-Missing").Should().BeEmpty();
-
-        // Multiple values are joined with ", "
         request.Headers.Add("X-Multi", "foo");
         request.Headers.Add("X-Multi", "bar");
+
+        // Act & assert
+        request.Headers.TryGetValue("X-Missing").Should().BeEmpty();
+        request.Headers.TryGetValue("X-Custom").Should().Be("value");
         request.Headers.TryGetValue("X-Multi").Should().Be("foo, bar");
     }
 }
