b39c0df72bb8865b5e53dd1cc5b8887acbf5ed13
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-19 13:33:12 +0300

    Update ThrottleLockTests.cs

--- PowerKit.Tests/ThrottleLockTests.cs
+++ PowerKit.Tests/ThrottleLockTests.cs
@@ -15,26 +15,26 @@ public class ThrottleLockTests
     {
         var interval = TimeSpan.FromMilliseconds(200);
         using var throttle = new ThrottleLock(interval);
-        var sw = Stopwatch.StartNew();
+        var stopwatch = Stopwatch.StartNew();
 
         // First call should not be throttled
         await throttle.WaitAsync();
-        sw.Elapsed.Should().BeLessThan(interval);
+        stopwatch.Elapsed.Should().BeLessThan(interval);
 
         // Subsequent call should be throttled for the remainder of the interval
         sw.Restart();
         await throttle.WaitAsync();
-        sw.Elapsed.Should().BeGreaterThanOrEqualTo(interval - TimeSpan.FromMilliseconds(50));
+        stopwatch.Elapsed.Should().BeGreaterThanOrEqualTo(interval - TimeSpan.FromMilliseconds(50));
 
         // After the interval elapses, the next call should not be throttled
         await Task.Delay(interval + TimeSpan.FromMilliseconds(50));
-        sw.Restart();
+        stopwatch.Restart();
         await throttle.WaitAsync();
-        sw.Elapsed.Should().BeLessThan(interval);
+        stopwatch.Elapsed.Should().BeLessThan(interval);
     }
 
     [Fact]
-    public async Task WaitAsync_Cancelled_ThrowsOperationCancelledException_Test()
+    public async Task WaitAsync_Cancellation_Test()
     {
         // Arrange
         using var throttle = new ThrottleLock(TimeSpan.FromSeconds(10));
@@ -44,16 +44,5 @@ public class ThrottleLockTests
         // Act & assert
         var act = async () => await throttle.WaitAsync(cts.Token);
         await act.Should().ThrowAsync<OperationCanceledException>();
-    }
-
-    [Fact]
-    public void Dispose_Test()
-    {
-        // Arrange
-        var throttle = new ThrottleLock(TimeSpan.FromSeconds(1));
-
-        // Act & assert
-        var act = throttle.Dispose;
-        act.Should().NotThrow();
     }
 }
