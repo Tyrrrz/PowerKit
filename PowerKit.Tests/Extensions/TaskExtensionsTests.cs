75919ba571ac025de6fe48104531116d8fab5e4b
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-07-22 21:02:19 +0300

    Update TaskExtensionsTests.cs

--- PowerKit.Tests/Extensions/TaskExtensionsTests.cs
+++ PowerKit.Tests/Extensions/TaskExtensionsTests.cs
@@ -9,7 +9,7 @@ namespace PowerKit.Tests.Extensions;
 public class TaskExtensionsTests
 {
     [Fact]
-    public async Task ObserveException_Task_ReturnsFaultException_Test()
+    public async Task ObserveException_ReturnsFaultException_Test()
     {
         // Arrange
         var task = Task.Run(() => throw new InvalidOperationException("test error"));
@@ -24,22 +24,7 @@ public class TaskExtensionsTests
     }
 
     [Fact]
-    public async Task ObserveException_TaskOfT_ReturnsFaultException_Test()
-    {
-        // Arrange
-        var task = Task.Run(new Func<int>(() => throw new InvalidOperationException("test error")));
-
-        // Act
-        var exception = await task.ObserveException();
-
-        // Assert
-        task.IsFaulted.Should().BeTrue();
-        exception.Should().NotBeNull();
-        exception!.InnerException.Should().BeOfType<InvalidOperationException>();
-    }
-
-    [Fact]
-    public async Task ObserveException_Task_SuccessfulTask_ReturnsNull_Test()
+    public async Task ObserveException_SuccessfulTask_ReturnsNull_Test()
     {
         // Arrange
         var task = Task.CompletedTask;
@@ -53,22 +38,7 @@ public class TaskExtensionsTests
     }
 
     [Fact]
-    public async Task ObserveException_TaskOfT_SuccessfulTask_ReturnsNull_Test()
-    {
-        // Arrange
-        var task = Task.FromResult(42);
-
-        // Act
-        var exception = await task.ObserveException();
-
-        // Assert
-        task.IsCompletedSuccessfully.Should().BeTrue();
-        (await task).Should().Be(42);
-        exception.Should().BeNull();
-    }
-
-    [Fact]
-    public async Task ObserveException_Task_DoesNotRaiseUnobservedTaskException_Test()
+    public async Task ObserveException_DoesNotRaiseUnobservedTaskException_Test()
     {
         // Arrange
         var unobservedRaised = false;
@@ -103,45 +73,6 @@ public class TaskExtensionsTests
         static void CreateFaultedTask()
         {
             Task.Run(() => throw new InvalidOperationException("test error")).ObserveException();
-        }
-    }
-
-    [Fact]
-    public async Task ObserveException_TaskOfT_DoesNotRaiseUnobservedTaskException_Test()
-    {
-        // Arrange
-        var unobservedRaised = false;
-        EventHandler<UnobservedTaskExceptionEventArgs> handler = (_, e) =>
-        {
-            if (e.Exception.InnerException is InvalidOperationException { Message: "test error" })
-                unobservedRaised = true;
-
-            e.SetObserved();
-        };
-        TaskScheduler.UnobservedTaskException += handler;
-
-        try
-        {
-            // Act
-            CreateFaultedTask();
-
-            await Task.Delay(50);
-
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
-
-            // Assert
-            unobservedRaised.Should().BeFalse();
-        }
-        finally
-        {
-            TaskScheduler.UnobservedTaskException -= handler;
-        }
-
-        static void CreateFaultedTask()
-        {
-            Task.Run(new Func<int>(() => throw new InvalidOperationException("test error"))).ObserveException();
         }
     }
 }
